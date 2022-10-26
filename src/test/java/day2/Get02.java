5ed2eb2838a9ccb5b0b9519191594dd824631d44
Sena Yucedal <sena.ycdl61@gmail.com>
2022-10-26 10:40:36 +0300

    sena

---
+++ src/test/java/day2/Get02.java
@@ -0,0 +1,44 @@
+package day2;
+
+import io.restassured.response.Response;
+import org.junit.Test;
+import static io.restassured.RestAssured.*;
+import static org.junit.Assert.*;
+
+public class Get02 {
+
+    /* Given
+    https://restful-booker.herokuapp.com/booking/1
+    When
+    User send a GET Request to the url
+    Then
+    HTTP Status code should be 404
+    And
+    Status Line should be HTTP/1.1 404 Not Found
+    And
+    Response body contains "Not Found"
+    And
+    Response body does not contain "TechProEd"
+    And
+    Server is "Cowboy"
+            */
+
+    @Test
+    public void get02(){
+
+        //set the URL
+        String url = "https://restful-booker.herokuapp.com/booking/1";
+
+        //set the expected Data
+
+        //type code to send request
+        Response response=given().when().get(url);
+        response.prettyPrint();
+
+        //do assertion
+        response.then().assertThat().statusCode(404).statusLine("HTTP/1.1 404 Not Found");
+        assertTrue(response.asString().contains("Not Found"));
+        assertFalse(response.asString().contains("TechProEd"));
+        assertEquals("Cowboy", response.getHeader("Server"));
+    }
+}
---
+++ src/test/java/day2/JsonplaceholderBaseUrl.java
@@ -0,0 +1,18 @@
+package day2;
+
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.specification.RequestSpecification;
+import org.junit.Before;
+
+public class JsonplaceholderBaseUrl {
+
+    protected RequestSpecification spec;
+
+    @Before
+    public void setup(){
+
+        spec=new RequestSpecBuilder().setBaseUri("https://jsonplaceholder.typicode.com").build();
+
+    }
+
+}
