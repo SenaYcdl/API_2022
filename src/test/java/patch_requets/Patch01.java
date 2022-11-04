3cfc9cdab003a06639dd038db9c33dec7981a047
Sena Yucedal <sena.ycdl61@gmail.com>
2022-11-04 22:28:49 +0300

    V18

--- src/test/java/patch_requets/Patch01.java
+++ src/test/java/patch_requets/Patch01.java
@@ -1,6 +1,61 @@
 package patch_requets;
 
-public class Patch01 {
+import base_urls.JsonplaceholderBaseUrl;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.Test;
+import test_data.JsonPlaceHolderTestData;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.Assert.assertEquals;
+
+public class Patch01 extends JsonplaceholderBaseUrl {
+
+     /*
+    Given
+     1) https://jsonplaceholder.typicode.com/todos/198
+     2) {
+             "title": "Wash the dishes"
+           }
+    When
+I send PATCH Request to the Url
+ Then
+      Status code is 200
+      And response body is like   {
+                   "userId": 10,
+                   "title": "Wash the dishes",
+                   "completed": true,
+                   "id": 198
+                   }
+ */
+
+    @Test
+    public void patch01(){
+
+        //set the url
+        spec.pathParams("first", "todos","second",198);
+
+        //set the expected data
+        JsonPlaceHolderTestData obj= new JsonPlaceHolderTestData();
+        Map<String,Object> expectedData = obj.expectedDataMethod(null,"Wash the dishes",null);
+        System.out.println("expectedData = " + expectedData);
+
+        //send the request and get the response
+        Response response=given().spec(spec).contentType(ContentType.JSON).body(expectedData).when().patch("/{first}/{second}");
+        response.prettyPrint();
+
+
+        //do assertion
+        Map<String,Object> actualData= response.as(HashMap.class);
+        System.out.println("actualData = " + actualData);
+
+        assertEquals(200,response.getStatusCode());
+        assertEquals(expectedData.get("title"),actualData.get("title"));
+
+
+    }
 
 }
