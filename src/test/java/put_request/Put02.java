8a9b6e4104b60485fc3f7d0bc634a4f1040046b4
Sena Yucedal <sena.ycdl61@gmail.com>
2022-11-10 23:45:28 +0300

    V28

---
+++ src/test/java/put_request/Put02.java
@@ -0,0 +1,56 @@
+package put_request;
+
+public class Put02 {
+
+    /*
+        URL: https://dummy.restapiexample.com/api/v1/update/21
+       HTTP Request Method: PUT Request
+       Request body: {
+                        "employee_name": "Ali Can",
+                        "employee_salary": 111111,
+                        "employee_age": 23,
+                        "profile_image": "Perfect image"
+                     }
+       Test Case: Type by using Gherkin Language
+       Assert:
+                i) Status code is 200
+                ii) Response body should be like the following
+                    {
+                        "status": "success",
+                        "data": {
+                            "employee_name": "Ali Can",
+                            "employee_salary": 111111,
+                            "employee_age": 23,
+                            "profile_image": "Perfect image"
+                        },
+                        "message": "Successfully! Record has been updated."
+                    }
+     */
+
+    /*
+    Given
+    URL: https://dummy.restapiexample.com/api/v1/update/21
+When
+    PUT Request
+      Request body: {
+                    "employee_name": "Ali Can",
+                    "employee_salary": 111111,
+                    "employee_age": 23,
+                    "profile_image": "Perfect image"
+                 }
+Then
+    i) Status code is 200
+And
+    ii) Response body should be like the following
+                {
+                    "status": "success",
+                    "data": {
+                        "employee_name": "Ali Can",
+                        "employee_salary": 111111,
+                        "employee_age": 23,
+                        "profile_image": "Perfect image"
+                    },
+                    "message": "Successfully! Record has been updated."
+                }
+     */
+}
